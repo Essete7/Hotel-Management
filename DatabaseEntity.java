3becd8e777fa5a659e5b0c848ca7dbffdc144e48
yoseph <haylishy@gmail.com>
2025-08-01 13:29:04 +0300

    databasemanger and other files added

---
+++ DatabaseEntity.java
@@ -0,0 +1,5 @@
+package com.hotel;
+
+public abstract class DatabaseEntity {
+    public abstract boolean saveToDatabase();
+}
---
+++ DatabaseManager.java
@@ -0,0 +1,23 @@
+package com.hotel;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseManager {
+    private static final String URL = "jdbc:postgresql://localhost:5432/hotel_db";
+    private static final String USER = "hotel_user";
+    private static final String PASSWORD = "hotel_pass";
+
+    static {
+        try {
+            Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException("PostgreSQL JDBC Driver not found", e);
+        }
+    }
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+}
